cf4041748a18a4c20ee4108f8c19324e861e9709
D6-80621-Kushagra-Khatri <144333385+kushagrakhatri@users.noreply.github.com>
2024-02-07 14:31:38 +0530

    all entities

---
+++ src/main/java/com/app/entities/FirstInformationReport.java
@@ -0,0 +1,26 @@
+package com.app.entities;
+
+import com.app.entities.end_users.InvestigatingOfficer;
+import com.app.entities.enums.Status;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class FirstInformationReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    @JoinColumn(name = "complaint_id")
+    @OneToOne
+    Complaint complaint;
+
+    @JoinColumn (name = "io_id")
+    @ManyToOne
+    private InvestigatingOfficer investigatingOfficer;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private Status status;
+}
---
+++ src/main/java/com/app/entities/enums/Title.java
@@ -0,0 +1,5 @@
+package com.app.entities.enums;
+
+public enum Title {
+	Mr,Mrs,Dr,Miss,Prof
+}
