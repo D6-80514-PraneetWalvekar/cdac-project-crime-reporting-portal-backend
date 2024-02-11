f13e4e55ddd0e37a9fbc7796ad44799c7cbb536f
D6-80621-Kushagra-Khatri <144333385+kushagrakhatri@users.noreply.github.com>
2024-02-11 15:03:09 +0530

    merge citizen endpoints and io sho

--- src/main/java/com/app/daos/ComplaintDao.java
+++ src/main/java/com/app/daos/ComplaintDao.java
@@ -1,7 +1,15 @@
 package com.app.daos;
 
-import com.app.entities.Complaint;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface ComplaintDao extends JpaRepository<Complaint, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.entities.Complaint;
+
+@Repository
+public interface ComplaintDAO extends JpaRepository<Complaint, Long> {
+
+//	List<Complaint> findAllById(Long citizen_id);
+
 }
